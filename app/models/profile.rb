c9439f73fc4f5331665de7cb1d6098dc4881e986
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-09 11:32:22 -0500

    all paperclip should be added to work

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,16 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  has_attached_file :image,
+    styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" },
+    default_url: "/images/:style/missing.png",
+    :storage => :s3,
+    :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
+    s3_region: ENV["AWS_REGION"]
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def s3_credentials
+    {:bucket => "travel-language-website-nycda", :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]}
+  end
+
 end
