9ec24fa4c8fcb0ba63ee80629a0b3f8c5d35deca
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-09 11:35:16 -0500

    rake migrate successful

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309004128) do
+ActiveRecord::Schema.define(version: 20170309163347) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "bio"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
