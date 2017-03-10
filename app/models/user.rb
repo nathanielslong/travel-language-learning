815e78654d6274b54b67b675c49863621bcb7fee
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-10 17:44:03 -0500

    user creation via the sign up form works

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,14 @@ class User < ApplicationRecord
   has_many :userlangs
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validat
+         :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :first_name, message: "Name must consist of only letters."
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :origin
+  validates_presence_of :destination
+  validates_presence_of :native_language
+  validates_presence_of :birthday
+  validates_presence_of :gender
 
 end
