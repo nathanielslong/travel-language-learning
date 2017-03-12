bb89af9c497fba8db86edb5c2b553694e22171ed
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-12 16:25:11 -0400

    adding messenging capacity to the database, as well as some corresponding views

---
+++ app/models/message.rb
@@ -0,0 +1,11 @@
+class Message < ActiveRecord::Base
+  belongs_to :conversation
+  belongs_to :user
+
+  validates_presence_of :body, :conversation_id, :user_id
+
+  def message_time
+    created_at.strftime(“%y/%m/%d at %l:%M %p”)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_one :profile
   has_many :userlangs
+  has_many :messages
+  has_many :conversations, :foreign_key => :sender_id
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :secure_validatable
