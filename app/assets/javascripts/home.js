d26be2055ea97aaab33e3d8f4425207f5d33d127
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-13 16:26:18 -0400

    map should be adding, but not initializing, code seems okay

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,7 @@
 $(window).load( function() {
   $('#myModal').modal('show');
+  initMap(0, 0);
+
   $("#pac-input").keyup(function(event){
     if(event.keyCode == 13){
       $("#modalButton").click();
@@ -10,7 +12,35 @@ $(window).load( function() {
     $('.close').click();
     var input = $('#pac-input').val();
     $('#secondInput').val(input);
+    codeAddress('pac-input')
   })
 } )
 
+function initMap(lat, lng) {
+  var location = {lat: lat, lng: lng};
+  console.log(location);
 
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: location,
+    zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
+
+function codeAddress(id) {
+  var address = document.getElementById(id).value;
+
+  var geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode( { 'address': address }, function(results, status) {
+    if (status == 'OK') {
+      initMap(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  } );
+}
