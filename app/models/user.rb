4bd6867f82d849efe61f95d5634042b3c304d52e
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-15 15:08:58 -0400

    added conversing users, and finished off suggested users method to populate the mailbox with people similar in terms of desired language of learning

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,11 +29,28 @@ class User < ApplicationRecord
   # end
   #
   def conversing_users
-    conversations = Conversation.where(sender_id: self.id).or(Conversation.where(recipient_id: self.id))
+    sender_conversations = Conversation.where(sender_id: self.id)
+    sender_users = User.find(sender_conversations.map(&:recipient_id))
+
+    recipient_conversations = Conversation.where(recipient_id: self.id)
+    recipient_users = User.find(recipient_conversations.map(&:sender_id))
+
+    users = sender_users + recipient_users
+
   end
 
   def languages_studying
-    Userlang.where(user_id: self.id).map(&:name)
+    user_languages = Userlang.where(user_id: self.id)
+
+    languages = Language.find(user_languages.map(&:language_id))
+
+    studying = []
+
+    languages.each do |language|
+      studying.push(language.name)
+    end
+
+    studying
   end
 
   def self.suggested_users(user)
@@ -41,7 +58,7 @@ class User < ApplicationRecord
 
     same_userlangs = Userlang.where(language_id: user_languages.map(&:language_id))
 
-    users = User.find(same_userlangs.map(&:user_id))
+    users = User.find(same_userlangs.map(&:user_id) - user.conversing_users.map(&:id))
   end
 
 end
