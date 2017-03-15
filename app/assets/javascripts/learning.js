f43160914f9cf54b202439614155d6772c2441fc
Chuong Nguyen <tinchuong@yahoo.fr>
2017-03-15 17:59:15 -0400

    Walk_Down done

--- app/assets/javascripts/learning.js
+++ app/assets/javascripts/learning.js
@@ -4,10 +4,20 @@ $(document).ready(function(){
      var station = 1; 
      $('#Walk_Right_btn').click(function () {
         rob = $('.Robert');
-        rob.addClass('Walk_Right').delay(4300).fadeIn(10, function() { rob.removeClass('Walk_Right').addClass('Robert_station' + station); station += 1;});
+    	if (station <= 4){
+        	rob.addClass('Walk_Right').delay(4300).fadeIn(10, function() { rob.removeClass('Walk_Right').addClass('Robert_station' + station); station += 1;});
+    	}
+    	if (station == 5){
+    		//rob.addClass('Walk_Right').delay(4300).fadeIn(10, function() { rob.removeClass('Walk_Right').addClass('Robert_station' + station); station += 1;});
+    		rob.addClass('Walk_Down').delay(3300).fadeIn(10, function() { rob.removeClass('Walk_Down').addClass('Robert_station' + station); station += 1;});
+    	}
+    	if (station > 5){
+    		//rob.addClass('Walk_Right').delay(4300).fadeIn(10, function() { rob.removeClass('Walk_Right').addClass('Robert_station' + station); station += 1;});
+    		rob.addClass('Walk_Left').delay(4300).fadeIn(10, function() { rob.removeClass('Walk_Left').addClass('Robert_station' + station); station += 1;});
 
-        console.log(station)
+    	}
     	
+    	console.log(station);	
     });
 
 
