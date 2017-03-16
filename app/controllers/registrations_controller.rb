815f3176c85a215d3f0d61df09d8f5023869a3ff
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-16 13:27:05 -0400

    removed routing to devise to push, will test again after push

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  def after_sign_up_path_for(resource)
-      after_sign_in_path_for(resource)
-  end
-
   private
 
   def sign_up_params
