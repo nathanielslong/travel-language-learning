da71800f119d3656fadc72b63e0c3d02fdf290f7
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-16 11:39:14 -0400

    adding regexes to the angular validations, also adding conditional logic for display of the submit button

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -15,7 +15,24 @@ google.maps.event.addDomListener(window, 'load', destinationAutoComplete);
 var myFormApp = angular.module('myFormApp', []);
 
 myFormApp.controller('mainController', function($scope) {
-  $scope.firstName= '';
+  $scope.firstName = '';
+  $scope.lastName = '';
   $scope.email = '';
   $scope.nameRegex = new RegExp(/^[a-zA-Z]+$/i);
+  $scope.emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 });
+
+$('form > input').keyup(function() {
+  var empty = false;
+  $('form > input').each(function() {
+    if ($(this).val() == '') {
+      empty = true;
+    }
+  })
+
+  if (empty) {
+    $('.btn-primary').attr('disabled', 'disabled');
+  } else {
+    $('.btn-primary').removeAttr('disabled');
+  }
+})
