2ba05bd66ee95127cd5b111e038717eee1c071e8
Dylan Richards <dylanerichards@gmail.com>
2017-03-16 11:14:15 -0400

    Remove extra blank line

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
 
   has_many :userlangs
   has_many :messages
-
   has_many :conversations, foreign_key: :sender_id
 
   devise :database_authenticatable, :registerable,
