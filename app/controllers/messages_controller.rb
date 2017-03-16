12c4bc8b623d9e731957c9c0152f5df8316de24c
Dylan Richards <dylanerichards@gmail.com>
2017-03-16 11:16:12 -0400

    Remvoe semicolon

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,17 +6,20 @@
 
   def index
     @messages = @conversation.messages
+
     if @messages.length > 10
       @over_ten = true
       @messages = @messages[-10..-1]
     end
+
     if params[:m]
       @over_ten = false
       @messages = @conversation.messages
     end
+
     if @messages.last
       if @messages.last.user_id != current_user.id
-        @messages.last.read = true;
+        @messages.last.read = true
       end
     end
 
@@ -35,6 +38,7 @@
   end
 
   private
+
   def message_params
     params.require(:message).permit(:body, :user_id)
   end
