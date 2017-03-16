1e6cf346f47ca37599d3e30fdc51e64509230ff9
Dylan Richards <dylanerichards@gmail.com>
2017-03-16 11:15:17 -0400

    Clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'learning/index'
+  get "learning/index"
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :conversations do
     resources :messages
   end
 
-  root to: 'home#index'
+  root "home#index"
 end
