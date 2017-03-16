ce74ef3adeebffe24fd30427cfe030468d12ecc9
Dylan Richards <dylanerichards@gmail.com>
2017-03-16 11:13:20 -0400

    Refactor classic conditional to ternary

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,7 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :trackable, :validatable
 
   def full_name
-    if first_name && last_name
-      first_name + " " + last_name
-    else
-      "Anonymous"
-    end
+    first_name && last_name ? "#{first_name} #{last_name}" : "Anonymous"
   end
 
   def conversing_users
