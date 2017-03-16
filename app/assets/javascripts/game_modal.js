917b38e5c3f04bc4c01d8cecbc930e4d9232ed15
Sam Almeida <samRAlmeida@icloud.com>
2017-03-16 15:03:28 -0400

    working on iframe

--- app/assets/javascripts/game_modal.js
+++ app/assets/javascripts/game_modal.js
@@ -5,7 +5,7 @@ $("document").ready(function(){
 $(".resume_game_button").on("click", function(){
 var modal = new Custombox.modal({
   content: {
-    effect: 'slip',
+    effect: 'flip',
     target: '.game_modal'
   }
 });
@@ -16,7 +16,14 @@ modal.open();
 })
 
 
+// var modal = new Custombox.modal({
+//   content: {
+//     effect: 'newspaper',
+//     target: '.game_modal'
+//   }
+// });
 
+// modal.open();
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -58,18 +58,26 @@ ActiveRecord::Schema.define(version: 20170312193919) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "origin"
+    t.string   "destination"
+    t.string   "native_language"
+    t.date     "birthday"
+    t.string   "gender"
+    t.boolean  "is_admin",               default: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
