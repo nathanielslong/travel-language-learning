1fd7f5dff61945ff55e4196e6125ec6b116659eb
Chuong Nguyen <tinchuong@yahoo.fr>
2017-03-16 01:19:29 -0400

    Walking functions completed, still need to fix errors when redo the trip

--- app/assets/javascripts/learning.js
+++ app/assets/javascripts/learning.js
@@ -5,18 +5,22 @@ $(document).ready(function(){
      $('#Walk_Right_btn').click(function () {
         rob = $('.Robert');
     	if (station <= 4){
-        	rob.addClass('Walk_Right').delay(4300).fadeIn(10, function() { rob.removeClass('Walk_Right').addClass('Robert_station' + station); station += 1;});
+        	rob.addClass('Walk_Right').delay(2800).fadeIn(10, function() { rob.removeClass('Walk_Right').addClass('Robert_station' + station); station += 1;});
     	}
     	if (station == 5){
     		//rob.addClass('Walk_Right').delay(4300).fadeIn(10, function() { rob.removeClass('Walk_Right').addClass('Robert_station' + station); station += 1;});
-    		rob.addClass('Walk_Down').delay(3300).fadeIn(10, function() { rob.removeClass('Walk_Down').addClass('Robert_station' + station); station += 1;});
+    		rob.addClass('Walk_Down').delay(2800).fadeIn(10, function() { rob.removeClass('Walk_Down').addClass('Robert_station' + station); station += 1;});
     	}
     	if (station > 5){
     		//rob.addClass('Walk_Right').delay(4300).fadeIn(10, function() { rob.removeClass('Walk_Right').addClass('Robert_station' + station); station += 1;});
-    		rob.addClass('Walk_Left').delay(4300).fadeIn(10, function() { rob.removeClass('Walk_Left').addClass('Robert_station' + station); station += 1;});
+    		rob.addClass('Walk_Left').delay(2800).fadeIn(10, function() { rob.removeClass('Walk_Left').addClass('Robert_station' + station); station += 1;});
 
     	}
-    	
+    	if (station > 8){
+    		station = 1;
+    		rob.removeClass('Robert_station1 Robert_station2 Robert_station3 Robert_station4 Robert_station5 Robert_station6 Robert_station7 Robert_station8');
+    		console.log("really: "+station);
+    	}
     	console.log(station);	
     });
 
