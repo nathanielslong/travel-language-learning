dc69d0e602fd42f05c81695f73a0a3a184961e46
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-16 09:44:29 -0400

    form stopped working by mouse, need to fix

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,5 +1,3 @@
-// Try to DRY up, it needs to work somehow
-
 function originAutoComplete() {
   var input = document.getElementById('origin');
   var autocomplete = new google.maps.places.Autocomplete(input);
@@ -10,23 +8,9 @@ function destinationAutoComplete() {
   var autocomplete = new google.maps.places.Autocomplete(input);
 }
 
-function locationAutoComplete() {
-  var input = document.getElementById('pac-input');
-  var autocomplete = new google.maps.places.Autocomplete(input);
-}
-
-function secondLocAuto() {
-  var input = document.getElementById('secondInput');
-  var autocomplete = new google.maps.places.Autocomplete(input);
-}
-
 google.maps.event.addDomListener(window, 'load', originAutoComplete);
 
 google.maps.event.addDomListener(window, 'load', destinationAutoComplete);
-
-google.maps.event.addDomListener(window, 'load', locationAutoComplete);
-
-google.maps.event.addDomListener(window, 'load', secondLocAuto);
 
 var myFormApp = angular.module('myFormApp', []);
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   get 'learning/index'
 
-  devise_for :users
-
   resources :conversations do
     resources :messages
   end
