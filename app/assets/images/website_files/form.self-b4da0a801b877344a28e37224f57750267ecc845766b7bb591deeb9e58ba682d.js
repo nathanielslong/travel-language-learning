0852e44a308545cce1dad590b6a6b245dc94f163
Sam Almeida <samRAlmeida@icloud.com>
2017-03-17 18:08:45 -0400

    attemting to render header

---
+++ app/assets/images/website_files/form.self-b4da0a801b877344a28e37224f57750267ecc845766b7bb591deeb9e58ba682d.js
@@ -0,0 +1,48 @@
+$(document).ready( function() {
+  $('.form-actions input').attr('disabled', 'disabled');
+
+  $('form input').keyup(function() {
+
+    var empty = false;
+    var invalid = false;
+    $('form input').each(function() {
+      if ($(this).val().length == 0) {
+        empty = true;
+      }
+    });
+
+    $('.validator .test').each(function() {
+      if (!$(this).hasClass('ng-hide')) {
+        invalid = true;
+      }
+    })
+
+    if (empty || invalid) {
+      $('.form-actions input').attr('disabled', 'disabled');
+        console.log(empty)
+    } else {
+      $('.form-actions input').removeAttr('disabled');
+    }
+  });
+} )
+
+function originAutoComplete() {
+  var input = document.getElementById('origin');
+  var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+function destinationAutoComplete() {
+  var input = document.getElementById('destination');
+  var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+google.maps.event.addDomListener(window, 'load', originAutoComplete);
+
+google.maps.event.addDomListener(window, 'load', destinationAutoComplete);
+
+var myFormApp = angular.module('myFormApp', []);
+
+myFormApp.controller('mainController', function($scope) {
+  $scope.nameRegex = new RegExp(/^[a-zA-Z]+$/i);
+  $scope.emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+});
---
+++ app/assets/images/website_files/home.self-2ca49b25b7eca46debff92cd3493d1f2cee630518853818241bf296b579c4a8e.js
@@ -0,0 +1,105 @@
+$(window).load( function() {
+  $('#myModal').modal('show');
+  initMap(61, 0);
+
+  $("#pac-input").keyup(function(event){
+    if(event.keyCode == 13){
+      $("#modalButton").click();
+    }
+  });
+
+  $('#modalButton').click(function() {
+    $('.close').click();
+    $('#offered').html("");
+    var input = $('#pac-input').val();
+    $('#secondInput').val(input);
+    codeAddress('pac-input');
+    getLanguage(input);
+  })
+
+  $("#secondInput").keyup(function(event){
+    if(event.keyCode == 13){
+      $("#secondButton").click();
+    }
+  });
+
+  $('#searchButton').click(function() {
+    $('#offered').html("");
+    var input = $('#secondInput').val();
+    codeAddress('secondInput');
+    getLanguage(input);
+  })
+
+})
+
+function locationAutoComplete() {
+  var input = document.getElementById('pac-input');
+  var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+function secondLocAuto() {
+  var input = document.getElementById('secondInput');
+  var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+google.maps.event.addDomListener(window, 'load', locationAutoComplete);
+
+google.maps.event.addDomListener(window, 'load', secondLocAuto);
+
+function initMap(lat, lng) {
+  if(lat == undefined) {
+    lat = 0;
+  }
+
+  if(lng == undefined) {
+    lng = 0;
+  }
+
+  var location = {lat: lat, lng: lng};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: location,
+    zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
+
+function codeAddress(id) {
+  var address = document.getElementById(id).value;
+
+  var geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode( { 'address': address }, function(results, status) {
+    if (status == 'OK') {
+      initMap(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  } );
+}
+
+languages = ['English', 'Portuguese', 'Vietnamese', 'French']
+
+function getLanguage(givenLocation) {
+  var formattedLocation = givenLocation.split(" ");
+
+  var country = formattedLocation[formattedLocation.length - 1];
+
+  $.ajax({
+    type: "GET",
+    url: "https://restcountries.eu/rest/v2/name/" + country,
+    success: function (d) {
+      $('#lang').val(d[0].languages[0].name);
+      if ($.inArray(d[0].languages[0].name, languages) != -1) {
+        $('#offered').html("We offer this language! Sign up today!");
+        $('.display-none').show();
+      } else {
+        $('#offered').html("We don't currently offer this language, but we're always adding more languages! Check back in a bit!");
+      }
+    }
+  });
+}
