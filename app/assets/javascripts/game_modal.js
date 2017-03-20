7d9322657443d1e77d231505d6f90aaede602a3e
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-20 09:35:34 -0400

    trying to fix page bugs

--- app/assets/javascripts/game_modal.js
+++ app/assets/javascripts/game_modal.js
@@ -2,36 +2,36 @@ $("document").ready(function(){
 
 
 
-$(".resume_game_button").on("click", function(){
-var modal = new Custombox.modal({
-  content: {
-    effect: 'contentscale',
-    target: '.game_modal'
-  }
-});
-modal.open();
-})
+  $(".resume_game_button").on("click", function(){
+    var modal = new Custombox.modal({
+      content: {
+        effect: 'contentscale',
+        target: '.game_modal'
+      }
+    });
+    modal.open();
+  })
 
-$(".message").on("click", function(){
-var modal = new Custombox.modal({
-  content: {
-    effect: 'blur',
-    target: '.msg_container'
-  }
-});
-modal.open();
-})
+  $(".message").on("click", function(){
+    var modal = new Custombox.modal({
+      content: {
+        effect: 'blur',
+        target: '.msg_container'
+      }
+    });
+    modal.open();
+  })
 
-$(".send_button").on("click", function(){
-	alert("Hi")
-var modal = new Custombox.modal({
-  content: {
-    effect: 'fadein',
-    target: '.msg_alert'
-  }
-});
-modal.open();
-})
+  $(".send_button").on("click", function(){
+    alert("Hi")
+    var modal = new Custombox.modal({
+      content: {
+        effect: 'fadein',
+        target: '.msg_alert'
+      }
+    });
+    modal.open();
+  })
 
 
 
